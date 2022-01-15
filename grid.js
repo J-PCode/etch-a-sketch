40b9caa6d1c748e65f092221e4c71064e38c44c4
J-PCode <orjala.juho@gmail.com>
2022-01-15 12:25:08 +0200

    added css to make things pretty, fixed issue with grid selection slider. this isn't responsive desing, and not 100% satisfied how it looks but well dont want to use my whole saturday for this.

--- grid.js
+++ grid.js
@@ -68,13 +68,6 @@ function removeDiv(){
 
 //javascript to run after functions and variables declared
 
-// here starts slider js
-const slider = document.getElementById("myRange");
-slider.oninput = function(){
-    grid(this.value)
-    rootJS();
-}
-// slider ends
 
 function rootJS(){    
 //Draw grid with selected color or basic color if not selected
@@ -85,6 +78,19 @@ draw.forEach(grid => {
 })
 })
 //draw grid ends
+
+//show slider values
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle also call functions grid and rooJS())
+slider.oninput = function() {
+  output.innerHTML = `${this.value} * ${this.value}`
+  grid(this.value)
+    rootJS();
+}
+//slider value ends
 
 //select color and reset buttons
 const button = document.querySelectorAll('button');
